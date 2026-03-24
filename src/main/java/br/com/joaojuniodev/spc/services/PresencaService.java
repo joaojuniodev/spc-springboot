07afb31c8f5ab7b007137e53e95739df614f6442
= <=>
2026-03-24 09:31:01 -0300

    subindo erro para testar

--- src/main/java/br/com/joaojuniodev/spc/services/PresencaService.java
+++ src/main/java/br/com/joaojuniodev/spc/services/PresencaService.java
@@ -20,7 +20,7 @@ public class PresencaService {
     private final Logger logger = LoggerFactory.getLogger(CatequizandoService.class.getName());
 
     @Autowired
-    private PresencaRepository repository;
+    private PresencaRepository repositoryyyy;
 
     @Autowired
     private CatequizandoRepository catequizandoRepository;
